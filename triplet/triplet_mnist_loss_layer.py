deb71ee4fcec3390531ce09ac1323fdfc1ed310e
thanhmvu <vut@lafayette.edu>
2018-03-02 14:06:12 -0600

    Clean up and comment

--- triplet/triplet_mnist_loss_layer.py
+++ triplet/triplet_mnist_loss_layer.py
@@ -31,18 +31,18 @@ class TripletMnistLoss(caffe.Layer):
     def forward(self, bottom, top):
         """Get blobs and copy them into this layer's top blob vector."""
         
-        anchor = np.array(bottom[0].data)
+        anchor = np.array(bottom[0].data)                                     # shape = (batch_size, feature_size)
         positive = np.array(bottom[1].data)
         negative = np.array(bottom[2].data)
         
-        aps = np.sum((anchor - positive) ** 2, axis=1)
+        aps = np.sum((anchor - positive) ** 2, axis=1)                        # shape = (batch_size)
         ans = np.sum((anchor - negative) ** 2, axis=1)
 
-        dist = self.margin + aps - ans
-        dist_hinge = np.maximum(dist, 0.0)
+        dist = self.margin + aps - ans                                        # shape = (batch_size)
+        dist_hinge = np.maximum(dist, 0.0)                                    # shape = (batch_size)
 
-        self.residual_list = np.asarray(dist_hinge > 0.0, dtype=np.float)
-        loss = np.sum(dist_hinge) / bottom[0].num        
+        self.residual_list = np.asarray(dist_hinge > 0.0, dtype=np.float)     # shape = (batch_size)
+        loss = np.sum(dist_hinge) / bottom[0].num                             # ex: 0.447827339172
 
         top[0].data[...] = loss
 
@@ -53,7 +53,7 @@ class TripletMnistLoss(caffe.Layer):
             positive = np.array(bottom[1].data)
             negative = np.array(bottom[2].data)
 
-            coeff = 2.0 * top[0].diff / bottom[0].num
+            coeff = 2.0 * top[0].diff / bottom[0].num                         # coeff = 0.03125
             bottom_a = coeff * \
                 np.dot(np.diag(self.residual_list), (negative - positive))
             bottom_p = coeff * \
@@ -61,7 +61,7 @@ class TripletMnistLoss(caffe.Layer):
             bottom_n = coeff * \
                 np.dot(np.diag(self.residual_list), (anchor - negative))
 
-            bottom[0].diff[...] = bottom_a
+            bottom[0].diff[...] = bottom_a                                    # shape = (batch_size, feature_size)
             bottom[1].diff[...] = bottom_p
             bottom[2].diff[...] = bottom_n
 
--- triplet/triplet_mnist_loss_layer0.py
+++ triplet/triplet_mnist_loss_layer0.py
@@ -2,36 +2,23 @@
 Triplet Mnist Loss Layer
 
 Author:Thanh Vu
-Date: Feb 3, 2018
+Date: Feb 9, 2018
 """
 
 import caffe
 import numpy as np
-# from random import randint
-# from random import shuffle
-# from PIL import Image
-# from tools import SimpleTransformer
-# from random import getrandbits as randBool
-# import sys
 
 
 class TripletMnistLoss(caffe.Layer):
   
-  # ==================================================================================== #
-  #                                        MAIN                                          #
-  # ==================================================================================== #
-  
   def setup(self, bottom, top):
     # Check top shape
-    if len(top) != 1:
-      raise Exception("Need to define 1 top blob (loss).")
+    assert len(top) == 1, "Top needs to have exactly 1 blob: loss."
     
     # Check bottom shape
-    if len(bottom) != 3:
-      raise Exception("Need to define 3 bottom blobs (feat_anchor, feat_pos, feat_neg).")
-
-    assert np.shape(bottom[0].data) == np.shape(bottom[1].data)
-    assert np.shape(bottom[0].data) == np.shape(bottom[2].data)
+    assert len(bottom) == 3, "Bottom needs to have exactly 3 blobs: feat_anchor, feat_pos, feat_neg."
+    assert np.shape(bottom[0].data) == np.shape(bottom[1].data), "bottom[0] and bottom[1] should have the same shape."
+    assert np.shape(bottom[0].data) == np.shape(bottom[2].data), "bottom[0] and bottom[2] should have the same shape."
 
     # Read parameters
     params = eval(self.param_str)
@@ -45,12 +32,6 @@ class TripletMnistLoss(caffe.Layer):
     """
     No reshape is needed at each pass because the input's size is fixed
     """
-    # void EuclideanLossLayer<Dtype>::Reshape(
-    # const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
-    # LossLayer<Dtype>::Reshape(bottom, top);
-    # CHECK_EQ(bottom[0]->count(1), bottom[1]->count(1))
-    #     << "Inputs must have the same dimension.";
-    # diff_.ReshapeLike(*bottom[0]);
     pass
     
     
@@ -58,62 +39,42 @@ class TripletMnistLoss(caffe.Layer):
     """
     Calculate loss
     """
-    batchLoss = float(0)
-    self.posLoss = []
-
+    total_loss = float(0)
+    self.pos_losses = []
     batch_size = bottom[0].num
+
     for i in range(batch_size):
+      anchor_feat = bottom[0].data[i]
+      pos_feat = bottom[1].data[i]
+      neg_feat = bottom[2].data[i]
 
-      anchor = np.array(bottom[0].data[i], dtype='f') # shape (channel, height, width)
-      pos = np.array(bottom[1].data[i], dtype='f')
-      neg = np.array(bottom[2].data[i], dtype='f')
-
-      ap_dist = np.sum((anchor - pos) ** 2)
-      an_dist = np.sum((anchor - neg) ** 2)
-
-      imgLoss = max(ap_dist - an_dist + self.margin, 0.0)
-      if imgLoss > 0: self.posLoss.append(i)
-
-      batchLoss += imgLoss
+      ap_sq_dist = np.sum((anchor_feat - pos_feat) ** 2)
+      an_sq_dist = np.sum((anchor_feat - neg_feat) ** 2)
+      img_loss = max(ap_sq_dist - an_sq_dist + self.margin, 0.0)
+      
+      if img_loss > 0: self.pos_losses.append(i)
+      total_loss += img_loss
    
-    avgLoss = batchLoss/batch_size ###### [?] divide by 2? divide by batch_size?
-    top[0].data[...] = avgLoss
+    avg_loss = total_loss / batch_size
+    top[0].data[...] = avg_loss
 
 
   def backward(self, top, propagate_down, bottom):
     """Get top diff and compute diff in bottom."""
     if propagate_down[0]:
       batch_size = bottom[0].num
-      for i in range(batch_size):
 
-        if i in self.posLoss:
-          anchor = np.array(bottom[0].data[i]) # shape (channel, height, width)
-          pos = np.array(bottom[1].data[i])
-          neg = np.array(bottom[2].data[i])
+      for i in range(batch_size):
+        if i in self.pos_losses:
+          anchor_feat = bottom[0].data[i]
+          pos_feat = bottom[1].data[i]
+          neg_feat = bottom[2].data[i]
           
-          bottom[0].diff[i] =  2*(pos - neg) ##### pos/neg of 1 img?
-          bottom[1].diff[i] =  -2*(anchor - pos)
-          bottom[2].diff[i] =  2*(anchor - neg)
+          bottom[0].diff[i] = 2 * (neg_feat - pos_feat) / batch_size
+          bottom[1].diff[i] = 2 * (pos_feat - anchor_feat) / batch_size
+          bottom[2].diff[i] = 2 * (anchor_feat - neg_feat) / batch_size
 
         else:
-          bottom[0].diff[i] = np.zeros((bottom[0].data)[1].shape)
-          bottom[1].diff[i] = np.zeros((bottom[0].data)[1].shape)
-          bottom[2].diff[i] = np.zeros((bottom[0].data)[1].shape)
-
-
-# def tripletLoss(pred, margin):
-#     a = pred[0::3]
-#     p = pred[1::3]
-#     n = pred[2::3]
-#     d1 = T.sum((a - p)**2, axis=-1)
-#     d2 = T.sum((a - n)**2, axis=-1)
-#     return T.mean(T.maximum(0, d1 + margin - d2))
-
-# with tf.name_scope("triplet_loss"):
-#         d_p_squared = tf.square(compute_euclidean_distance(anchor_feature, positive_feature))
-#         d_n_squared = tf.square(compute_euclidean_distance(anchor_feature, negative_feature))
-
-#         loss = tf.maximum(0., d_p_squared - d_n_squared + margin)
-#         #loss = d_p_squared - d_n_squared + margin
-
-#         return tf.reduce_mean(loss), tf.reduce_mean(d_p_squared), tf.reduce_mean(d_n_squared)
+          bottom[0].diff[i] = 0.0
+          bottom[1].diff[i] = 0.0
+          bottom[2].diff[i] = 0.0
